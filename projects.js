6d57854b9abc1b42c234e1a0ed7dd69d44c7ff3c
Fabian <110415215+LetsBePhoenix@users.noreply.github.com>
2025-10-28 12:32:18 +0100

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,228 @@
+//Projekt:
+// Vileon
+// FlameCraft
+// ModCraft
+// LenoxServer
+// TorchCraft
+//
+// Typ:
+// Mods
+// Plugins
+// Vanilla
+// Vanilla+
+//
+// Modus:
+// Survival
+// Creative
+//
+// Speciality:
+// YouTube
+// Coming SOON
+
+const projects = [
+    {
+        id: 'planed-2',
+        name: 'TorchCraft 3 - RPG',
+        from: 'COMING SOON',
+        to: 'ca. 3 Monate',
+        participants: ['KamiKatze', 'LetsPhoenix'],
+        short: '',
+        archived: false,
+        tags: ['TorchCraft', 'Plugins', 'Survival', 'Coming SOON'],
+        images: [],
+        cover: 'https://pbs.twimg.com/media/EhKd2R2UYAAhHbA.jpg'
+    },
+    {
+        id: 'planed-1',
+        name: 'ModCraft 3',
+        from: 'COMING SOON',
+        to: 'ca. 3 Monate',
+        participants: [],
+        short: '',
+        archived: false,
+        tags: ['ModCraft', 'Mods', 'Survival', 'Coming SOON'],
+        images: [],
+        cover: 'https://pbs.twimg.com/media/EhKd2R2UYAAhHbA.jpg'
+    },
+    {
+        id: 'project-15',
+        name: 'FlameCraft 6',
+        from: 'Unbekannt',
+        to: 'Unbekannt',
+        participants: [],
+        short: 'Ein Projekt, das relativ spontan entstanden ist, leider aber auch kaum zulauf hatte. PixelPalle baute eine Villa mit den neuen Blöcken. Julipixi baute ein Haus und Phoenix baute ein Strandhaus. \n' +
+            'Startete in Suvival aber am Ende war der Server tot und die übrigen Spieler (Builder) wollten auf Kreativ modus wechseln.',
+        archived: true,
+        tags: ['FlameCraft', 'Vanilla+', 'Survival', 'Creative'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    },
+    {
+        id: 'project-14',
+        name: 'FlameCraft 5 - FeuerTee',
+        from: '06.2024',
+        to: '11.2024',
+        participants: ['Teestreuer', 'LetsPhoenix', 'Freya', 'Silja', 'PixelPalle', 'Gog', 'Sopie', 'KamiKatze', 'Lasse'],
+        short: 'Ein Projekt, welches entstand, da Teestreuer und Phoenix Lust auf Minecraft hatten. Es sind immer mehr Personen dazu gekommen, und so ist das Projekt gewachsen. Teestreuer baute ein Wikinger-Dorf, daneben baute Phoenix ein dunkles Dorf, und Freya baute in der Nähe ein paar größere Häuschen. Domi und Gog fingen etwas weiter weg eine Villa an. Kami und Lasse legten eine Fläche für einen Tempel frei. Sopie war etwas weiter weg, und baute ein kleines Haus mit einem Stall.',
+        archived: true,
+        tags: ['FlameCraft', 'Vanilla+', 'Survival'],
+        images: ['https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/FeuerTee/TeesBase.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/FeuerTee/PhoenixBase2.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/FeuerTee/PhoenixBase.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/FeuerTee/FreyaBase.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/FeuerTee/FreyaSchildi1.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/FeuerTee/FreyaSchildi2.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/FeuerTee/FreyaChicken.png'],
+        cover: 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/FeuerTee/PhoenixBase.png'
+    },
+    {
+        id: 'project-13',
+        name: 'FlameCraft 4',
+        from: '06.2023',
+        to: '07.2023',
+        participants: ['KamiKatze', 'Teestreuer', 'LetsPhoenix', 'Lenox'],
+        short: 'Ein Projekt mit vielen Fehlern. Teestreuer baute ein kleines Wickinger-Camp. Phoenix baute ein dunkles Dorf. Kami und Lenox bauten in einem CherryGrove.',
+        archived: true,
+        tags: ['FlameCraft', 'Vanilla', 'Survival'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    },
+    {
+        id: 'project-12',
+        name: 'FlameCraft 3',
+        from: '11.2022',
+        to: '01.2023',
+        participants: ['PixelPalle', 'Gog', 'LetsPhoenix', 'Freya', 'Silja', 'Ramona', 'KamiKatze', 'Lenox', 'JuliPixi', 'Vincent', 'Timon', 'Merle'],
+        short: 'Ein Projekt, das auf Wunsch weniger gemacht wurde, diese aber fast nie online waren. Dafür haben andere das Projekt sehr bereichert. Domi und Gog bauten eine im Berg hängende Basis. Phoenix baute eine Insel zu einer Weihnachtsinsel um. Vincent und Julipixi bauten ein kleines Dorf. Freya und Killing bauten eine kreisförmige Basis. Star, Lenox und Kami bauten ein detailliertes Dörfchen. Anita baute ein riesiges Loch.',
+        archived: true,
+        tags: ['FlameCraft', 'Vanilla+', 'Survival'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    },
+    {
+        id: 'project-11',
+        name: 'FlameCraft 2',
+        from: 'Unbekannt',
+        to: 'Unbekannt',
+        participants: ['KamiKatze', 'Teestreuer', 'LetsPhoenix', 'Lenox', 'Ramona', 'Freya', 'Silja'],
+        short: 'Ein Projekt, wo viel Liebe und Inspiration reinfloss. Teestreuer baute ein 1:1 Replika des Schiffs Bismarck. Star, Phoenix und Lenox bauten ein Dorf. Freya und Killing bauten in einem bergigen Dorf.',
+        archived: true,
+        tags: ['FlameCraft', 'Vanilla+', 'Survival'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    },
+    {
+        id: 'project-10',
+        name: 'FlameCraft 1 - The lost Season',
+        from: 'Unbekannt',
+        to: 'Unbekannt',
+        participants: ['LetsPhoenix'],
+        short: 'Leider ist mir und anderen nicht mehr bekannt, was der Inhalt und die Bauwerke dieses servers wahren.',
+        archived: true,
+        tags: ['FlameCraft', 'Vanilla+', 'Survival'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    },
+    {
+        id: 'project-9',
+        name: 'TorchCraft 2',
+        from: '02.2022',
+        to: '06.2022',
+        participants: ['KamiKatze', 'Teestreuer', 'LetsPhoenix', 'JuliPixi', 'Gog', 'Lenox', 'Timon'],
+        short: 'Auf Nachfrage entstand mit mehr Leuten TC2. Phoenix baute eine Kirche mit großem Keller. Domi und Julipixi ließen sich in der Nähe nieder und bauten auch jeweils ein Haus. Endcrafter baute eine große unterirdische Basis und Anita baute eine große detaillierte Burg.',
+        archived: true,
+        tags: ['TorchCraft', 'Plugins', 'Survival'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    },
+    {
+        id: 'project-8',
+        name: 'TorchCraft 1 - Covid',
+        from: 'Unbekannt',
+        to: 'Unbekannt',
+        participants: ['KamiKatze', 'Teestreuer', 'LetsPhoenix', 'Loni', 'Vico'],
+        short: 'Phoenix hatte Covid19 und deswegen zu viel Zeit... Ein Minecraft-Server mit einigen nützlichen Plugins. Alle bauten um einen See herum. Endcrafter baute ein großes Baumhaus, welches er nach kurzer Zeit abbrannte. Kami baute eine schöne Höhle und Lenox baute einige Farmen. Star und Phoenix verschrieben sich dem Dorfprojekt... Phoenix plättete einen großen Berg und Star verschönerte das Gebiet.',
+        archived: true,
+        tags: ['TorchCraft', 'Plugins', 'Survival'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    },
+    {
+        id: 'project-7',
+        name: 'Lenox - Asia',
+        from: '08.2021',
+        to: '08.2021',
+        participants: ['Lenox', 'KamiKatze', 'Teestreuer', 'LetsPhoenix'],
+        short: 'Lenox wollte nach längerem mal wieder einen Standard-Minecraft-Server. Kami baute ein Villager-Dorf um, Phoenix baute einen Tempel mit Tempelanlage, Lenox grub mehrere Chunks aus und alle halfen Teestreuer ein Wickinger-Dorf zu bauen.',
+        archived: true,
+        tags: ['LenoxServer', 'Vanilla', 'Survival'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    },
+    {
+        id: 'project-6',
+        name: 'ModCraft 2 - White Tower / Magic',
+        from: '05.2021',
+        to: 'Unbekannt',
+        participants: ['KamiKatze', 'Teestreuer', 'LetsPhoenix', 'Timon', 'Vico'],
+        short: 'Dadurch dass ModCraft 1 so gut lief, entschloss sich Phoenix direkt einen Nachfolger zu machen. Ein an Magie angehauchtes Modpack. Dadurch ist ein großer weißer Turm entstanden. (An mehr erinnere ich mich leider nicht)',
+        archived: true,
+        tags: ['ModCraft', 'Mods', 'Survival'],
+        images: ['https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft2/PhoenixLoadstone.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft2/EndcrafterDrache.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft2/Schlonk.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft2/Bone.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft2/Twilight.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft2/R%C3%BCstungen.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft2/InaktivMana.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft2/AktivMana.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft2/Irgendwas.png'],
+        cover: 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft2/EndcrafterDrache.png'
+    },
+    {
+        id: 'project-5',
+        name: 'ModCraft - Factory',
+        from: '03.2021',
+        to: '04.2021',
+        participants: ['KamiKatze', 'Timon', 'Vico', 'Bartsch', 'LetsPhoenix'],
+        short: 'Phoenix hatte mal Lust etwas anderes auszuprobieren. Begonnen mit einem kleinen Häuschen, stand nach kurzer Zeit ein riesiges Industriegelände. Ebenfalls ist weiter weg noch ein schönes Dorf in den Bergen entstanden.',
+        archived: true,
+        tags: ['ModCraft', 'Mods', 'Survival'],
+        images: ['https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/MapPart1.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/MapPart2.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/StarterBase.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Factory_1.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Factory_2.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Factory_3.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Factory_4.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Factory_5.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Factory_Inside1.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Factory_Inside2.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Factory_Inside3.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Factory_Inside4.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Facory_XP-Farm1.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Facory_XP-Farm2.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/FlyingTower_1.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/FlyingTower_2.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/FlyingTower_3.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/FlyingTower_4.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/FlyingTower_5.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/FlyingTower_6.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/FlyingTower_7.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/IceTower.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/NetherCastle.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/DragonBreeder.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/DrachenAngriff.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/IceSpikeMinigame_1.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/IceSpikeMinigame_2.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/IceSpikeMinigame_3.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Relyser_Base.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/SecondVillage_1.png', 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/SecondVillage_2.png'],
+        cover: 'https://letsbephoenix.github.io/LetsPhoenix.github.io/gallerie/ModCraft1%20-%20Factory/Factory_2.png'
+    },
+    {
+        id: 'project-4',
+        name: 'Vileon 2 - ForestVillage',
+        from: '01.2021',
+        to: '03.2021',
+        participants: ['KamiKatze', 'Teestreuer', 'LetsPhoenix'],
+        short: 'Der bisher letzte Versuch ein gemeinsames YouTube-Projekt zu machen. Ein Dorfspawn in einem Zauberwald.',
+        archived: true,
+        tags: ['Vileon', 'Vanilla', 'Survival', 'YouTube'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    },
+    {
+        id: 'project-3',
+        name: 'Vileon 2 - Redsand',
+        from: '01.2021',
+        to: '01.2021',
+        participants: ['Teestreuer','KamiKatze','LetsPhoenix'],
+        short: 'Ein YouTube-Projekt, das zu monoton wurde (Inspiration fehlte). Eine Insel mit einer direkt daneben liegenden Rotsand-Wüste, welche perfekt zum Ressourcensammeln geeignet war.',
+        archived: true,
+        tags: ['Vileon', 'Vanilla', 'Survival', 'YouTube'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    },
+    {
+        id: 'project-2',
+        name: 'Lenox - Castle',
+        from: '12.2020',
+        to: '01.2021',
+        participants: ['Lenox','Gog', 'PixelPalle', 'Teestreuer', 'LetsPhoenix'],
+        short: 'Der erste richtige always-on Server mit Freunden. Ein Server zum entspannten gemeinsamen Zocken. Es wurde gebaut und gehandelt. Es gab ein großes Team, welches eine Burg, eine riesige Scheune, eine Bank und vieles mehr baute. Ebenfalls gab es zwei Spieler, die sich nicht dem Team angeschlossen haben, sondern selber bauen wollten.',
+        archived: true,
+        tags: ['LenoxServer', 'Vanilla', 'Survival'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    },
+    {
+        id: 'project-1',
+        name: 'Vileon 1 - Public',
+        from: '10.2020',
+        to: '12.2020',
+        participants: ['LetsPhoenix', 'KamiKatze', 'Teestreuer'],
+        short: 'Ein Server für die Öffentlichkeit, auf dem Spieler zusammenarbeiten konnten, um eine große Stadt zu errichten. Die Stadt wuchs schnell und es entstanden viele interessante Gebäude und Projekte.',
+        archived: true,
+        tags: ['Vileon', 'Vanilla', 'Survival', 'YouTube'],
+        images: [],
+        cover: 'https://mcutils.com/display/packpng.svg'
+    }
+];
